2036888ac820d520f13977f81309ce811680d6ec
Shayan Shirahmadi Gale Bagi <sshirahm@gra-login2.graham.sharcnet>
2022-10-27 18:35:57 -0400

    Working on CRMF

---
+++ train.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --time=04:00:00
+#SBATCH --mem=10G
+#SBATCH --account=rrg-mcrowley
+#SBATCH --gres=gpu:t4:1
+#SBATCH --cpus-per-task=11
+#SBATCH --job-name=crmf             
+#SBATCH --output=crmf_%j.log
+#SBATCH --mail-user=shayan.shirahmadi@gmail.com
+#SBATCH --mail-type=ALL
+
+python train.py
