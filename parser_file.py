212950402f6229e93ed1f4a805362cf1ac506d77
Shayan <shayan.shirahmadi@gmail.com>
2023-01-21 20:04:44 -0500

    Added bash files

--- parser_file.py
+++ parser_file.py
@@ -9,7 +9,7 @@ def get_parser():
     parser.add_argument("--tfdir", default="", type=str)
     parser.add_argument("--dataset_name", default="eth", type=str)
     parser.add_argument("--model_name", default="lstm", type=str)
-    parser.add_argument("--resume", default="./models/E40/CRMF_epoch_880.pth.tar",
+    parser.add_argument("--resume", default="",
                         type=str, metavar="PATH", help="path to latest checkpoint (default: none)")
 
     # randomness
