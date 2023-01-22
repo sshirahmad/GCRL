a58fcd13353451be3dfb9ce2e8c1be5f129573e6
Shayan <shayan.shirahmadi@gmail.com>
2023-01-22 06:23:08 -0500

    Added bash files

--- scripts/train_eth_ucy.sh
+++ scripts/train_eth_ucy.sh
@@ -12,9 +12,9 @@ domain_shifts='1-2-4-8-64'
 DATA="--dataset_name $dataset --add_confidence --domain_shifts $domain_shifts"
 MODEL="--model_name $model"
 DIR="--tfdir runs/$dataset/$exp/"
-TRAINING="--num_epochs $e --batch_size $bs --batch_method $batch_method"
 bs=64
 dim=8
 e=300
+TRAINING="--num_epochs $e --batch_size $bs --batch_method $batch_method"
 
 CUDA_VISIBLE_DEVICES=$GPU python train.py $DEFAULTS $DATA $TRAINING $DIR $MODEL --z_dim $dim --s_dim $dim --seed 1
