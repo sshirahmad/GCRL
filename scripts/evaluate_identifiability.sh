8c9e3243f070a9975baf1511aa67e404afbb91af
Shayan <shayan.shirahmadi@gmail.com>
2023-01-24 22:37:11 -0500

    Added bash files

--- scripts/evaluate_identifiability.sh
+++ scripts/evaluate_identifiability.sh
@@ -1,4 +1,4 @@
-# Used for Domain Generalization Experiment
+# Used for Identifiability
 
 # PRETRAIN MODELS (v4 MLP)
 GPU=0 # 0. Set GPU
@@ -8,12 +8,13 @@ DEFAULTS="--decoupled_loss --coupling --no-rel_recon"
 EVALUATION="--metrics accuracy"
 dataset="v4"
 model="mlp"
-dset_type="val"
+dset_type="test"
 bs=64
 f_envs='0.1-0.3-0.5'
 DATA="--dataset_name $dataset --filter_envs $f_envs --batch_size $bs --reduceall 9000 --dset_type $dset_type"
 z_dim=2
 num_envs=5
+mcc='weak'
 
 epoch=250
 paths=()
@@ -21,4 +22,4 @@ for seed in 1 2 3 4 5
 do
      paths+=("models/$dataset/$exp/VCRL_data_${dataset}_ds_0_bk_20_ns_10_ep_${epoch}_seed_${seed}_cl_True_dc_True_latentdim_${z_dim}_cluster_${num_envs}.pth.tar")
 done
-CUDA_VISIBLE_DEVICES=$GPU python identifiability.py $DEFAULTS $DATA $EVALUATION --model_name $model --resume "${paths[@]}" --seed $seed
+CUDA_VISIBLE_DEVICES=$GPU python identifiability.py $DEFAULTS $DATA $EVALUATION --model_name $model --paths "${paths[@]}" --mcc $mcc --seed $seed
