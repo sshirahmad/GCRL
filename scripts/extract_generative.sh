26608da092fe6402ae78c36d84b88a771039e7bc
Shayan <shayan.shirahmadi@gmail.com>
2023-01-24 21:30:29 -0500

    Added bash files

---
+++ scripts/extract_generative.sh
@@ -0,0 +1,22 @@
+# Used for Best_N
+
+dataset="v4"
+exp='pretrain'
+
+mkdir -p results/$dataset/$exp
+echo 'N, split, shifts, envs, seed, ADE, FDE' | tee results/$dataset/$exp/summary_bestN.csv
+
+for filename in log/$dataset/$exp/*.log; do
+	cat ${filename} \
+	| grep "Best_N:\|Split:\|Shifts:\|Envs:\|Seed:\|ADE:" \
+	| sed "s/.*Best_N: \([0-9]*\).*/\1,/g" \
+	| sed "s/.*Split: \([a-z.]*\).*/\1,/g" \
+	| sed "s/.*Shifts: \([0-9]*\).*/\1,/g" \
+	| sed "s/.*Envs: \([0-9.-]*\).*/\1,/g" \
+	| sed "s/.*Seed: \([0-9]*\).*/\1,/g" \
+	| sed "s/.*ADE: \([0-9.]*\).*FDE: \([0-9.]*\).*/\1, \2 /g" \
+	| paste -d " " - - - - - - - \
+	| tee -a results/$dataset/$exp/summary_bestN.csv
+done
+
+python visualize.py --exp $exp --dataset_name $dataset
