e2efd6617e3b773a1bf8919b123be2a5c0e955f4
zahrag <zahra.gharaee@gmail.com>
2023-01-25 16:54:41 -0500

    Experiments

--- visualize.py
+++ visualize.py
@@ -10,10 +10,225 @@ import pandas as pd
 import os
 import io
 import warnings
-from visualization import exp_StyleDomainShift_IM, exp_DomianAdaptation_IM_seeds12345
-
 
 from utils import NUMBER_PERSONS, set_name_method, set_name_env, set_name_finetune
+
+
+def exp_StyleDomainShift_IM():
+
+    """
+        In this function Domain Generalization experiments' results by style-domain-shifts
+        of 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8 at inference for
+        5 seeds; 1,2,3,4,5 are presented for IM-Synthetic dataset with IM model.
+        :return ADE-MEAN, ADE-STD, FDE-MEAN, FDE-STD:
+        """
+
+    # ##### EIM (seed = 1) epochs=470 ##### #
+    ADE_EIM_S1_ds1 = 0.0498
+    FDE_EIM_S1_ds1 = 0.0708
+
+    ADE_EIM_S1_ds2 = 0.0576
+    FDE_EIM_S1_ds2 = 0.0731
+
+    ADE_EIM_S1_ds3 = 0.0611
+    FDE_EIM_S1_ds3 = 0.0777
+
+    ADE_EIM_S1_ds4 = 0.0686
+    FDE_EIM_S1_ds4 = 0.0865
+
+    ADE_EIM_S1_ds5 = 0.0806
+    FDE_EIM_S1_ds5 = 0.0995
+
+    ADE_EIM_S1_ds6 = 0.1056
+    FDE_EIM_S1_ds6 = 0.1300
+
+    ADE_EIM_S1_ds7 = 0.1522
+    FDE_EIM_S1_ds7 = 0.1868
+
+    ADE_EIM_S1_ds8 = 0.2174
+    FDE_EIM_S1_ds8 = 0.2671
+
+    # ##### EIM_S2 (seed = 2) epochs=470 ##### #
+    ADE_EIM_S2_ds1 = 0.0484
+    FDE_EIM_S2_ds1 = 0.0694
+
+    ADE_EIM_S2_ds2 = 0.0534
+    FDE_EIM_S2_ds2 = 0.0692
+
+    ADE_EIM_S2_ds3 = 0.0575
+    FDE_EIM_S2_ds3 = 0.0732
+
+    ADE_EIM_S2_ds4 = 0.0671
+    FDE_EIM_S2_ds4 = 0.0838
+
+    ADE_EIM_S2_ds5 = 0.0794
+    FDE_EIM_S2_ds5 = 0.0996
+
+    ADE_EIM_S2_ds6 = 0.1090
+    FDE_EIM_S2_ds6 = 0.1317
+
+    ADE_EIM_S2_ds7 = 0.1578
+    FDE_EIM_S2_ds7 = 0.1842
+
+    ADE_EIM_S2_ds8 = 0.2253
+    FDE_EIM_S2_ds8 = 0.2575
+
+    # ##### EIM_S3 (seed = 3) epochs=470 ##### #
+    ADE_EIM_S3_ds1 = 0.0515
+    FDE_EIM_S3_ds1 = 0.0767
+
+    ADE_EIM_S3_ds2 = 0.0550
+    FDE_EIM_S3_ds2 = 0.0771
+
+    ADE_EIM_S3_ds3 = 0.0582
+    FDE_EIM_S3_ds3 = 0.0779
+
+    ADE_EIM_S3_ds4 = 0.0696
+    FDE_EIM_S3_ds4 = 0.0845
+
+    ADE_EIM_S3_ds5 = 0.0752
+    FDE_EIM_S3_ds5 = 0.0961
+
+    ADE_EIM_S3_ds6 = 0.1034
+    FDE_EIM_S3_ds6 = 0.1240
+
+    ADE_EIM_S3_ds7 = 0.1494
+    FDE_EIM_S3_ds7 = 0.1669
+
+    ADE_EIM_S3_ds8 = 0.2091
+    FDE_EIM_S3_ds8 = 0.2255
+
+    # ##### EIM_S4 (seed = 4) epochs=470 ##### #
+    ADE_EIM_S4_ds1 = 0.0486
+    FDE_EIM_S4_ds1 = 0.0825
+
+    ADE_EIM_S4_ds2 = 0.0536
+    FDE_EIM_S4_ds2 = 0.0805
+
+    ADE_EIM_S4_ds3 = 0.0572
+    FDE_EIM_S4_ds3 = 0.0840
+
+    ADE_EIM_S4_ds4 = 0.0689
+    FDE_EIM_S4_ds4 = 0.0939
+
+    ADE_EIM_S4_ds5 = 0.0768
+    FDE_EIM_S4_ds5 = 0.1022
+
+    ADE_EIM_S4_ds6 = 0.1001
+    FDE_EIM_S4_ds6 = 0.1231
+
+    ADE_EIM_S4_ds7 = 0.1460
+    FDE_EIM_S4_ds7 = 0.1672
+
+    ADE_EIM_S4_ds8 = 0.2107
+    FDE_EIM_S4_ds8 = 0.2339
+
+    # ##### EIM_S5 (seed = 5) epochs=470 ##### #
+    ADE_EIM_S5_ds1 = 0.0490
+    FDE_EIM_S5_ds1 = 0.0717
+
+    ADE_EIM_S5_ds2 = 0.0594
+    FDE_EIM_S5_ds2 = 0.0755
+
+    ADE_EIM_S5_ds3 = 0.0624
+    FDE_EIM_S5_ds3 = 0.0796
+
+    ADE_EIM_S5_ds4 = 0.0696
+    FDE_EIM_S5_ds4 = 0.0879
+
+    ADE_EIM_S5_ds5 = 0.0799
+    FDE_EIM_S5_ds5 = 0.1008
+
+    ADE_EIM_S5_ds6 = 0.1094
+    FDE_EIM_S5_ds6 = 0.1320
+
+    ADE_EIM_S5_ds7 = 0.1670
+    FDE_EIM_S5_ds7 = 0.1959
+
+    ADE_EIM_S5_ds8 = 0.2383
+    FDE_EIM_S5_ds8 = 0.2782
+
+    ADE_seeds_ds1 = [ADE_EIM_S1_ds1, ADE_EIM_S2_ds1, ADE_EIM_S3_ds1, ADE_EIM_S4_ds1, ADE_EIM_S5_ds1]
+    FDE_seeds_ds1 = [FDE_EIM_S1_ds1, FDE_EIM_S2_ds1, FDE_EIM_S3_ds1, FDE_EIM_S4_ds1, FDE_EIM_S5_ds1]
+    m_ade1, m_fde1, s_ade1, s_fde1 = get_mean_std_over_seeds(ADE_seeds_ds1, FDE_seeds_ds1, ds=0.1, model='IM')
+
+    ADE_seeds_ds2 = [ADE_EIM_S1_ds2, ADE_EIM_S2_ds2, ADE_EIM_S3_ds2, ADE_EIM_S4_ds2, ADE_EIM_S5_ds2]
+    FDE_seeds_ds2 = [FDE_EIM_S1_ds2, FDE_EIM_S2_ds2, FDE_EIM_S3_ds2, FDE_EIM_S4_ds2, FDE_EIM_S5_ds2]
+    m_ade2, m_fde2, s_ade2, s_fde2 = get_mean_std_over_seeds(ADE_seeds_ds2, FDE_seeds_ds2, ds=0.2, model='IM')
+
+    ADE_seeds_ds3 = [ADE_EIM_S1_ds3, ADE_EIM_S2_ds3, ADE_EIM_S3_ds3, ADE_EIM_S4_ds3, ADE_EIM_S5_ds3]
+    FDE_seeds_ds3 = [FDE_EIM_S1_ds3, FDE_EIM_S2_ds3, FDE_EIM_S3_ds3, FDE_EIM_S4_ds3, FDE_EIM_S5_ds3]
+    m_ade3, m_fde3, s_ade3, s_fde3 = get_mean_std_over_seeds(ADE_seeds_ds3, FDE_seeds_ds3, ds=0.3, model='IM')
+
+    ADE_seeds_ds4 = [ADE_EIM_S1_ds4, ADE_EIM_S2_ds4, ADE_EIM_S3_ds4, ADE_EIM_S4_ds4, ADE_EIM_S5_ds4]
+    FDE_seeds_ds4 = [FDE_EIM_S1_ds4, FDE_EIM_S2_ds4, FDE_EIM_S3_ds4, FDE_EIM_S4_ds4, FDE_EIM_S5_ds4]
+    m_ade4, m_fde4, s_ade4, s_fde4 = get_mean_std_over_seeds(ADE_seeds_ds4, FDE_seeds_ds4, ds=0.4, model='IM')
+
+    ADE_seeds_ds5 = [ADE_EIM_S1_ds5, ADE_EIM_S2_ds5, ADE_EIM_S3_ds5, ADE_EIM_S4_ds5, ADE_EIM_S5_ds5]
+    FDE_seeds_ds5 = [FDE_EIM_S1_ds5, FDE_EIM_S2_ds5, FDE_EIM_S3_ds5, FDE_EIM_S4_ds5, FDE_EIM_S5_ds5]
+    m_ade5, m_fde5, s_ade5, s_fde5 = get_mean_std_over_seeds(ADE_seeds_ds5, FDE_seeds_ds5, ds=0.5, model='IM')
+
+    ADE_seeds_ds6 = [ADE_EIM_S1_ds6, ADE_EIM_S2_ds6, ADE_EIM_S3_ds6, ADE_EIM_S4_ds6, ADE_EIM_S5_ds6]
+    FDE_seeds_ds6 = [FDE_EIM_S1_ds6, FDE_EIM_S2_ds6, FDE_EIM_S3_ds6, FDE_EIM_S4_ds6, FDE_EIM_S5_ds6]
+    m_ade6, m_fde6, s_ade6, s_fde6 = get_mean_std_over_seeds(ADE_seeds_ds6, FDE_seeds_ds6, ds=0.6, model='IM')
+
+    ADE_seeds_ds7 = [ADE_EIM_S1_ds7, ADE_EIM_S2_ds7, ADE_EIM_S3_ds7, ADE_EIM_S4_ds7, ADE_EIM_S5_ds7]
+    FDE_seeds_ds7 = [FDE_EIM_S1_ds7, FDE_EIM_S2_ds7, FDE_EIM_S3_ds7, FDE_EIM_S4_ds7, FDE_EIM_S5_ds7]
+    m_ade7, m_fde7, s_ade7, s_fde7 = get_mean_std_over_seeds(ADE_seeds_ds7, FDE_seeds_ds7, ds=0.7, model='IM')
+
+    ADE_seeds_ds8 = [ADE_EIM_S1_ds8, ADE_EIM_S2_ds8, ADE_EIM_S3_ds8, ADE_EIM_S4_ds8, ADE_EIM_S5_ds8]
+    FDE_seeds_ds8 = [FDE_EIM_S1_ds8, FDE_EIM_S2_ds8, FDE_EIM_S3_ds8, FDE_EIM_S4_ds8, FDE_EIM_S5_ds8]
+    m_ade8, m_fde8, s_ade8, s_fde8 = get_mean_std_over_seeds(ADE_seeds_ds8, FDE_seeds_ds8, ds=0.8, model='IM')
+
+    means_ade = [m_ade1, m_ade2, m_ade3, m_ade4, m_ade5, m_ade6, m_ade7, m_ade8]
+    stds_ade = [s_ade1, s_ade2, s_ade3, s_ade4, s_ade5, s_ade6, s_ade7, s_ade8]
+
+    means_fde = [m_fde1, m_fde2, m_fde3, m_fde4, m_fde5, m_fde6, m_fde7, m_fde8]
+    stds_fde = [s_fde1, s_fde2, s_fde3, s_fde4, s_fde5, s_fde6, s_fde7, s_fde8]
+
+    return means_ade, means_fde, stds_ade, stds_fde
+
+
+def get_mean_std_over_seeds(ADE_seeds, FDE_seeds, ds=0.6, model='IM'):
+
+    ave_ADE_over_seeds = np.mean(ADE_seeds)
+    ave_FDE_over_seeds = np.mean(FDE_seeds)
+
+    print('******* MODEL is {}'.format(model))
+    print(f'Mean ADE over seeds with test domain shift of {ds:g}:', ave_ADE_over_seeds)
+    print(f'Mean FDE over seeds with test domain shift of {ds:g}:', ave_FDE_over_seeds)
+
+    std_ADE_over_seeds = np.std(ADE_seeds)
+    std_FDE_over_seeds = np.std(FDE_seeds)
+
+    print(f'STD ADE over seeds with test domain shift of {ds:g}:', std_ADE_over_seeds)
+    print(f'STD FDE over seeds with test domain shift of {ds:g}:', std_FDE_over_seeds)
+
+    return ave_ADE_over_seeds, ave_FDE_over_seeds, std_ADE_over_seeds, std_FDE_over_seeds
+
+
+def exp_DomianAdaptation_IM_seeds12345():
+
+    """
+    In this function Domain Adaptation experiments' results by fine-tuning for 6 batches at inference for
+    5 seeds; 1,2,3,4,5 are presented for IM-Synthetic dataset with IM model.
+    :return ADE-MEAN, ADE-STD, FDE-MEAN, FDE-STD:
+
+    # \Finetune:
+    reduce       64x0=0     64x1   64x2=128    64x3=192    64x4=256    64x5=320    64x6=384
+    ADE mean     0.106      0.102  0.098       0.096       0.096       0.094       0.092
+         std     0.004      0.005  0.004       0.004       0.004       0.003       0.004
+     FDE mean    0.128      0.123  0.120       0.118       0.118       0.115       0.114
+         std     0.004      0.005  0.005       0.005       0.005       0.005       0.005
+    """
+
+    m_ade_IM = [0.106,  0.102,  0.098,  0.096,  0.096,  0.094,  0.092]
+    s_ade_IM = [0.004,  0.005,  0.004,  0.004,  0.004,  0.003,  0.004]
+
+    m_fde_IM = [0.128,  0.123,  0.120,  0.118,  0.118,  0.115,  0.114]
+    s_fde_IM = [0.004,  0.005,  0.005,  0.005,  0.005,  0.005,  0.005]
+
+    return m_ade_IM, s_ade_IM, m_fde_IM, s_fde_IM
 
 
 def main(args):
