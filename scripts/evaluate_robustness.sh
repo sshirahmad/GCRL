2483b98b773b450ad8c57476ffbe126ffd432ddf
Shayan <shayan.shirahmadi@gmail.com>
2023-01-25 16:40:47 -0500

    Added bash files

--- scripts/evaluate_robustness.sh
+++ scripts/evaluate_robustness.sh
@@ -18,5 +18,5 @@ for shift in "8" "16" "32" "64"
 do
     DATA="--dataset_name $dataset --domain_shifts $shift --add_confidence --batch_size $bs --dset_type $dset_type"
     seed=1
-    CUDA_VISIBLE_DEVICES=$GPU python evaluate_model.py $DEFAULTS $DATA $EVALUATION --model_name $model --resume "models/$dataset/$exp/VCRL_data_${dataset}_ds_${domain_shifts}_bk_1_ns_10_ep_${epoch}_seed_${seed}_cl_True_dc_False_latentdim_${z_dim}_cluster_${num_envs}_epoch_${epoch}.pth.tar" --z_dim $z_dim --s_dim $z_dim --seed $seed
+    CUDA_VISIBLE_DEVICES=$GPU python evaluate_model.py $DEFAULTS $DATA $EVALUATION --model_name $model --resume "models/$dataset/$exp/GCRL_data_${dataset}_ds_${domain_shifts}_bk_1_ns_10_ep_${epoch}_seed_${seed}_cl_True_dc_False_latentdim_${z_dim}_cluster_${num_envs}_epoch_${epoch}.pth.tar" --z_dim $z_dim --s_dim $z_dim --seed $seed
 done
